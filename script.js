53aec42394a8c348400456374d0ad6e76f849d4a
Eric Valderas <antikampa@gmail.com>
2026-09-01 19:10:00 +0200

    beutiful

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ var song = document.getElementById("mySong");
 const audioElement = document.querySelector("audio");
 
 
-  function togglePlay() {
-     
-
-    if (song.paused) {
-      song.play();
-    } else {
-      song.pause();
+ function togglePlay() {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     }
-}
 
 
